68af35d5200f73cfc5d379a2744a1aefa8bd0a0a
ou.yun <ou.yun@tci-cn.co.jp>
2019-05-10 19:43:13 +0900

    change port to 8088, mock server port 3004

--- src/vuex/api/services/products.js
+++ src/vuex/api/services/products.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 export default {
   getAll (request = {}) {
-    return axios.get('http://localhost:3000/products', request)
+    return axios.get('http://localhost:3004/products', request)
           .then((response) => Promise.resolve(response))
           .catch((error) => Promise.reject(error))
   }
